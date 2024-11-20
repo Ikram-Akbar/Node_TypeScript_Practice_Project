82e0abf00368191ebb1bcb697a25a7b48b5ae994
Ikram Akbar <ikramakbar71@gmail.com>
2024-11-20 21:49:17 +0600

    server intialise with typescript and others necessary tools

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from "./app";
+import config from "./app/config";
+import mongoose from 'mongoose';
+
+async function main() {
+   
+    try {
+        await mongoose.connect(config.MONGODB_URL as string);
+        app.listen(config.PORT, () => {
+            console.log(`Example app listening on port ${config.PORT}`)
+        })
+    }
+    catch (error) {
+        console.error(`Error connecting to MongoDB: ${error}`);
+    }
+}
+
